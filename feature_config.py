ff569cb5746032771635905bee7d4374b3bb95e2
Dewa Ferrouzi <dewa.ferrouzi41@ui.ac.id>
2025-01-05 19:42:27 +0700

    Finished initial streamlit webapp

--- feature_config.py
+++ feature_config.py
@@ -1,8 +1,21 @@
 # feature_config.py
 
+# feature_config.py
+
 FEATURE_TYPES = {
-    "jumlah_laba_komprehensif", "per", "roa", "roe",
-        "net_profit_margin", "opm", "eps", "book_value", 
-        "pbv", "aset_idr", "liabilitas_idr", "ekuitas_idr", 
-        "total_pendapatan_idr", "der"
+    "jumlah_laba_komprehensif": "numerical",
+    "per": "numerical",
+    "roa": "numerical",
+    "roe": "numerical",
+    "net_profit_margin": "numerical",
+    "opm": "numerical",
+    "eps": "numerical",
+    "book_value": "numerical",
+    "pbv": "numerical",
+    "aset_idr": "numerical",
+    "liabilitas_idr": "numerical",
+    "ekuitas_idr": "numerical",
+    "total_pendapatan_idr": "numerical",
+    "der": "numerical"
 }
+
